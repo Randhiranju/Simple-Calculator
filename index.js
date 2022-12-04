e772b1c16f2c6e6c322e63cac9caf0c6c7388d78
Randhir <randhiranju9355@gmail.com>
2022-12-04 19:36:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let answer = document.getElementById("answer");
+
+inputs = document.querySelectorAll(".buttons input");
+let screenvalue = '';
+inputs.forEach((inp) => {
+    inp.addEventListener("click", (e) => {
+        // console.log(e);
+        text = e.target.value;
+        console.log(text);
+
+        if(text == "AC"){
+            screenvalue = '';
+            answer.value = screenvalue;
+        }
+        else if(text == "="){
+            answer.value = eval(screenvalue);
+        }
+        else if(text == "DEL"){
+            answer.value = answer.value.substr(0, answer.value.length - 1); // paarth
+        }
+        else{
+            screenvalue += text;
+            answer.value = screenvalue;
+        }
+    })
+});
